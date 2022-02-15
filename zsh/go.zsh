7194de302753d33d848b1be127f0fd44df19839a
Rob Crowe <nobby.crowe@gmail.com>
2022-02-15 21:19:04 +0000

    GOPRIVATE=go.buf.build

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,7 +1,7 @@
 export GOPATH=$HOME/go
 
 # Ignore work modules from hitting proxy
-export GOPRIVATE=github.com/utilitywarehouse/*
+export GOPRIVATE=go.buf.build,github.com/utilitywarehouse/*
 
 # Add go built binaries to path
 export PATH=$GOPATH/bin:$PATH

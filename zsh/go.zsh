23ce97d39f0ac8dd0e582eecb26ed217ad102c81
Rob Crowe <nobby.crowe@gmail.com>
2025-10-18 10:16:56 +0100

    enable direnv

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,7 +1,7 @@
 export GOPATH=$HOME/go
 
 # Ignore work modules from hitting proxy
-export GOPRIVATE=go.buf.build,github.com/thirdfort/*
+export GOPRIVATE=go.buf.build,github.com/thirdfort/*,github.com/namespacelabs/*
 
 # Add go built binaries to path
 export PATH=$GOPATH/bin:$PATH

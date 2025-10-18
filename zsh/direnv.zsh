415dae1f4587d8263f6273ad7a088b19cc882e8e
Rob Crowe <nobby.crowe@gmail.com>
2025-10-18 10:41:09 +0100

    suppress direnv use flake noise

--- zsh/direnv.zsh
+++ zsh/direnv.zsh
@@ -1,1 +1,5 @@
-eval "$(direnv hook bash)"
+export DIRENV_LOG_FORMAT="$(printf "\033[2mdirenv: %%s\033[0m")"
+eval "$(direnv hook zsh)"
+_direnv_hook() {
+  eval "$(direnv export zsh 2> >(egrep -v -e '^....direnv: export' >&2))"
+};

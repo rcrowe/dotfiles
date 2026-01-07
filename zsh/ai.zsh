a252ddb199f4526d80d502bed31c5c53a27410d7
Rob Crowe <282571+rcrowe@users.noreply.github.com>
2026-01-07 10:03:33 +0000

    Setup ampcode

--- zsh/ai.zsh
+++ zsh/ai.zsh
@@ -1,6 +1,2 @@
-# Direnv configuration
-if command -v wt &> /dev/null; then
-    eval "$(command wt config shell init zsh)";
-else
-    echo "'wt' (worktrunk) could not be found. See https://github.com/max-sixty/worktrunk"
-fi
+# Amp
+export PATH="/Users/rcrowe/.amp/bin:$PATH"
